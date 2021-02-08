21f2b23d99a302b054d2ce3b5d5bacaa6fb91d24
phueb <info@philhuebner.com>
2021-02-08 15:37:35 -0600

    add silhouette score

--- README.md
+++ README.md
@@ -6,14 +6,22 @@ Research code for evaluating category knowledge acquired by neural networks.
 
 ## Usage
 
-### Clustering Performance
+### Classification Performance
 
-A measure how well learned representations can be clustered to confirm to a gold category structure
+A measure how well pairs of learned representations can be correctly judged to belong to the same category
 
 ### Divergence-from-Prototype
 
 A measure of how abstract learned representations are.
 
+### Category-Spread 
+
+A measure of spread between learned representations that belong to the same category
+
+### Silhouette Score
+
+A measure of how well the similarity structure of learned representations captures the gold category structure 
+
 ## Compatibility
 
-Developed on Ubuntu 16.04 and Python 3.7.6
+Developed on Ubuntu 18.04 and Python 3.7
---
+++ categoryeval/si.py
@@ -0,0 +1,32 @@
+from typing import List, Dict, Set, Optional
+import numpy as np
+from sklearn.metrics.cluster import silhouette_score
+
+from categoryeval import configs
+from categoryeval.probestore import ProbeStore
+
+
+class SIScorer:
+    def __init__(self,
+                 corpus_name: str,
+                 probes_names: List[str],
+                 w2id: Dict[str, int],
+                 excluded: Optional[Set[str]] = None,
+                 ) -> None:
+        print('Initializing SIScorer...')
+
+        assert len(probes_names) == len(set(probes_names))
+
+        self.probes_names = probes_names
+        self.name2store = {probes_name: ProbeStore(corpus_name, probes_name, w2id, excluded)
+                           for probes_name in probes_names}
+
+    def calc_score(self,
+                   representations: np.array,
+                   category_labels: List[int],
+                   ):
+        """
+        """
+        print(f'Computing silhouette scores...')
+
+        return silhouette_score(representations, category_labels)
